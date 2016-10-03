d66941060543339378da8fd7ef66d35388c9ceff
ayush-webonise <ayush.mahajan@weboniselab.com>
2016-10-03 19:20:43 +0530

    Update AppDelegate.h

--- TableViewApplication/AppDelegate.h
+++ TableViewApplication/AppDelegate.h
@@ -11,7 +11,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) UINavigationController *navigationController;
-@property (strong, nonatomic) UIViewController *viewController;
+@property (strong, nonatomic) UIViewController *rootVC;
 @property (readwrite) NSMutableArray *arrayProducts;
 @end
 
